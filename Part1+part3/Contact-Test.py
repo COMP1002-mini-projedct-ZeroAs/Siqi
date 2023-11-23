5552715d283042c897296ad7452a04f58772fffe
William Wen <18909852201@163.com>
2023-11-24 00:45:42 +0800

    Update Contact-Test.py
    add part2

--- Part1+part3/Contact-Test.py
+++ Part1+part3/Contact-Test.py
@@ -20,6 +20,8 @@ class Conncact(Info):
     tagContList=[]
     contDict={}
     noneList=[]
+    coverOption=False
+    coverName=''
     contDict.update(dict(JSON.parse(conTemp)))
     tagDict={'{{systemDefault':['{{Everyone']}
     def __init__(self,name,phone,email,tag,remarks):
@@ -49,15 +51,32 @@ class Conncact(Info):
             return Conncact.eamil_check(self)
     def phone_check(self):
         phone=input("Input your phone:")
-        PhoCheck=re.search("\D",phone) 
-        if PhoCheck==None and phone!="":
-            Conncact.phoneList.append(phone)
-            if Conncact.continue_Judge()==True:
-                return Conncact.phone_check(self)
+        condiction=False
+        for name in Conncact.getPhone():
+            if phone in Conncact.getPhone()[name]:
+                condiction=True
+                break
+        print(name)    
+        if condiction==True:            
+            print(f"This phone:{phone} already exist or recreate?")
+            option=Conncact.phoneoptioninput()
+            if option=='Recreate':
+                return Conncact.output(self)
+            elif option=='Cover':
+                coverOption=True
+                coverName=name
+                Conncact.phoneList.append(phone)
+                return Conncact.phoneList,coverName,coverOption
+        else:               
+            PhoCheck=re.search("\D",phone) 
+            if PhoCheck==None and phone!="":
+                Conncact.phoneList.append(phone)
+                if Conncact.continue_Judge()==True:
+                    return Conncact.phone_check(self)
+                else:
+                    return Conncact.phoneList
             else:
-                return Conncact.phoneList
-        else:
-            return Conncact.phone_check(self)
+                return Conncact.phone_check(self)
     def tagCont_edit():
         tagCont=input("Input tag Contant:")
         if tagCont=="":
@@ -86,26 +105,77 @@ class Conncact(Info):
         if item in range(1,len(list1)+1):
             list1.pop(item-1)
         else:
-            return Conncact.itemdel(self,list1)        
-    def output(self):
+            return Conncact.itemdel(self,list1)  
+    def optioninput():
+        option=input("Recreate, Cover or Merge?").capitalize()
+        if option=='Merge':
+            return option
+        elif option=='Recreate':
+            return option
+        elif option=='Cover':
+            return option
+        else:
+            return Conncact.optioninput() 
+    def phoneoptioninput():
+        option=input("Recreate or Cover?").capitalize()
+        if option=='Recreate':
+            return option
+        elif option=='Cover':
+            return option
+        else:
+            return Conncact.optioninput()         
+    def getPhone():
+        phoneList={}
+        for name in Conncact.contDict:
+            for phone in Conncact.contDict[name]['Phone-num']:
+                if phone not in list(phoneList.values()):
+                    if name in list(phoneList.keys()):
+                        phoneList[name].append(phone)
+                    else:
+                        tempDict={name:[phone]}
+                        phoneList.update(tempDict)        
+        return phoneList                                
+    def output(self):                                       #Create connact
         self.n=input("Input name:")
-        self.p=Conncact.phone_check(self)
+        if self.n in Conncact.contDict:
+            print("This name already exist, recreate or merge?")
+            operateType=Conncact.optioninput()
+            if operateType=='Recreate':
+                return Conncact.output(self)
+            elif operateType=='Cover':
+                Conncact.coverOption=True 
+                Conncact.coverName=self.n   
+            elif operateType=='Merge':
+                partList=['Phone-num','Email','Tag','Remark']
+                for part in partList:
+                    Conncact.updateInfo(self,self.n,part)
+                return    
+        self.p,name,condition=Conncact.phone_check(self)
         self.e=Conncact.eamil_check(self)
         self.t=Conncact.tag_edit(self)
         self.r=input("Input remark:")
         cont={self.n:{f'Name':self.n,'Phone-num':self.p,'Email':self.e,'Tag':self.t,'Remark':self.r}}
-        Conncact.contDict.update(cont)
+        return cont,name,condition
+    def fileinput(self):
+        tempdict,name,condition=Conncact.output(self)
+        if condition==True:
+            print(name)
+            Conncact.contDict.pop(name)
+        Conncact.contDict.update(tempdict)
         f=open("C:\connect\connect.txt","w+")
         f.write(str(Conncact.contDict))
         f.close()
+        f=open("C:\connect\connect.txt","r")
+        cont=f.read()
+        f.close()
+        Conncact.contDict=dict(JSON.parse(cont))
         return Conncact.contDict
     def getInfo(self):
         for person in Conncact.contDict:
             Conncact.personInfo.append(Conncact.contDict[person])
         return Conncact.personInfo
     def infoList(self,name):
-        pos=list(Conncact.infodict.keys()).index(name)
-        infoDict=Conncact.getInfo(self)[pos]
+        infoDict=Conncact.contDict[name]
         self.n=infoDict['Name']
         self.p=infoDict['Phone-num']
         self.e=infoDict['Email']
@@ -129,7 +199,7 @@ class Conncact(Info):
                 print(*self.t[tag],end=" ")
         print()        
         print(f"Remarks:{self.r}")        
-    def printall(self):
+    def printall(self):                         #print all information
         for name in Conncact.contDict:
             Conncact.printInfo(self,name) 
             print()      
@@ -150,8 +220,9 @@ class Conncact(Info):
         else:
             return Conncact.tagCheck(tagDict)
                           
-    def updateInfo(self,name):
-        part=input("Input which part you want to update:").capitalize()
+    def updateInfo(self,name,part=None):                                                  #update information
+        if part==None:
+            part=input("Input which part you want to update:").capitalize()   
         if part not in ['Name','Phone-num','Email','Tag','Remark']:
             return Conncact.updateInfo(self,name)     
         else:    
@@ -177,8 +248,8 @@ class Conncact(Info):
                     else:
                         Conncact.itemdel(infoDict[part][changeTag])    
                 else:
-                     changeTag=Conncact.tagCheck(infoDict[part])
-                     infoDict[part].pop(changeTag)  
+                    changeTag=Conncact.tagCheck(infoDict[part])
+                    infoDict[part].pop(changeTag)  
         if Conncact.continue_Judge==True:
             return Conncact.updateInfo(self,name)
         else:
@@ -221,7 +292,7 @@ class Conncact(Info):
                     Conncact.noneList.append(name)
                         
         return nameList
-    def tagClassify(self,tagtype):
+    def tagClassify(self,tagtype):                              #tagClassify
         for tagCont in Conncact.tagGet(self,tagtype):
             print(f'{tagCont}:')
             for name in Conncact.taggfilter(self,tagtype,tagCont):
@@ -272,7 +343,7 @@ class Conncact(Info):
                 nameset.append(set1)
         return nameset        
             
-    def multifilterPrint(self):
+    def multifilterPrint(self):                     #filter
         nameset=Conncact.multiFilter(self)
         set1=nameset.pop(0)
         nameList=list(set1.intersection(*nameset))
@@ -283,8 +354,13 @@ class Conncact(Info):
         else:
             print("None")        
 con1=Conncact(1,1,1,1,1)
-con1.filterPrint('Location','China') 
-con1.output()
-con1.multifilterPrint()
+#for name in Conncact.getPhone():
+    #print(list(Conncact.getPhone()[name]))
+#con1.updateInfo('Zhang')
+#con1.filterPrint('Location','China') 
+con1.fileinput()
+con1.printall()
+#写代码的是小丑，超级大傻逼，代码写的像答辩。
+#写part4的是大佬，代码超级美观，安全，高效
 
     
